c01e80bc88e914ba59d12725ccf2d74e3a496589
diego <oliveiraj.diego@gmail.com>
2024-02-28 14:30:23 -0300

    feat: add SQS queue policies

--- main.tf
+++ main.tf
@@ -1,6 +1,22 @@
+data "aws_iam_policy_document" "iam_sqs_send_policy" {
+  statement {
+    sid    = "send_message"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = ["arn:aws:sqs:us-east-1:720502424322:*"]
+  }
+}
+
 resource "aws_sqs_queue" "fila_pagamento_aprovado" {
   name          = var.fila_pagamento_aprovado
   delay_seconds = 60
+  policy        = data.aws_iam_policy_document.iam_sqs_send_policy.json
 
   tags = {
     Environment = "techchallenge"
@@ -11,6 +27,7 @@ resource "aws_sqs_queue" "fila_pagamento_aprovado" {
 resource "aws_sqs_queue" "fila_pagamento_reprovado" {
   name          = var.fila_pagamento_reprovado
   delay_seconds = 60
+  policy        = data.aws_iam_policy_document.iam_sqs_send_policy.json
 
   tags = {
     Environment = "techchallenge"
@@ -21,6 +38,7 @@ resource "aws_sqs_queue" "fila_pagamento_reprovado" {
 resource "aws_sqs_queue" "fila_pagamento_cancelado" {
   name          = var.fila_pagamento_cancelado
   delay_seconds = 60
+  policy        = data.aws_iam_policy_document.iam_sqs_send_policy.json
 
   tags = {
     Environment = "techchallenge"
@@ -31,6 +49,7 @@ resource "aws_sqs_queue" "fila_pagamento_cancelado" {
 resource "aws_sqs_queue" "fila_pedido_pronto_fifo" {
   name                        = var.fila_pedido_pronto_fifo
   delay_seconds               = 60
+  policy                      = data.aws_iam_policy_document.iam_sqs_send_policy.json
   fifo_queue                  = true
   content_based_deduplication = true
 
@@ -43,6 +62,7 @@ resource "aws_sqs_queue" "fila_pedido_pronto_fifo" {
 resource "aws_sqs_queue" "fila_producao_fifo" {
   name                        = var.fila_producao_fifo
   delay_seconds               = 60
+  policy                      = data.aws_iam_policy_document.iam_sqs_send_policy.json
   fifo_queue                  = true
   content_based_deduplication = true
 
@@ -55,6 +75,7 @@ resource "aws_sqs_queue" "fila_producao_fifo" {
 resource "aws_sqs_queue" "fila_pagamento_pendente" {
   name          = var.fila_pagamento_pendente
   delay_seconds = 60
+  policy        = data.aws_iam_policy_document.iam_sqs_send_policy.json
 
   tags = {
     Environment = "techchallenge"
@@ -95,15 +116,31 @@ resource "aws_sns_topic_subscription" "topico_producao_fifo_sqs_producao_fifo" {
   topic_arn = aws_sns_topic.topico_producao_fifo.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.fila_producao_fifo.arn
+
+  filter_policy = <<POLICY
+  {
+    "status": ["EM_PREPARACAO"]
+  }
+  POLICY
+
+  filter_policy_scope = "MessageBody"
 }
 
 resource "aws_sns_topic_subscription" "topico_producao_fifo_sqs_pedido_pronto_fifo" {
   topic_arn = aws_sns_topic.topico_producao_fifo.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.fila_pedido_pronto_fifo.arn
+
+  filter_policy = <<POLICY
+  {
+    "status": ["PRONTO"]
+  }
+  POLICY
+
+  filter_policy_scope = "MessageBody"
 }
 
-resource "aws_sns_topic_subscription" "topico_pagamento_retorno_sqs_pagamento_pendente" {
+resource "aws_sns_topic_subscription" "topico_pagamento_pendente_sqs_pagamento_pendente" {
   topic_arn = aws_sns_topic.topico_pagamento_pendente.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.fila_pagamento_pendente.arn
@@ -113,17 +150,40 @@ resource "aws_sns_topic_subscription" "topico_pagamento_retorno_sqs_pagamento_ap
   topic_arn = aws_sns_topic.topico_pagamento_retorno.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.fila_pagamento_aprovado.arn
+
+  filter_policy = <<POLICY
+  {
+    "status": ["RECEBIDO"]
+  }
+  POLICY
+
+  filter_policy_scope = "MessageBody"
 }
 
 resource "aws_sns_topic_subscription" "topico_pagamento_retorno_sqs_pagamento_reprovado" {
   topic_arn = aws_sns_topic.topico_pagamento_retorno.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.fila_pagamento_reprovado.arn
+
+  filter_policy = <<POLICY
+  {
+    "status": ["FINALIZADO"]
+  }
+  POLICY
+
+  filter_policy_scope = "MessageBody"
 }
 
 resource "aws_sns_topic_subscription" "topico_pagamento_retorno_sqs_pagamento_cancelado" {
   topic_arn = aws_sns_topic.topico_pagamento_retorno.arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.fila_pagamento_cancelado.arn
-}
 
+  filter_policy = <<POLICY
+  {
+    "status": ["CANCELADO"]
+  }
+  POLICY
+
+  filter_policy_scope = "MessageBody"
+}
